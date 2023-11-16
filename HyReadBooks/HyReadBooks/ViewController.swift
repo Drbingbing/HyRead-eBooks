f6cf9465e431efb01cec7cdc0a026091096e3275
Drbingbing <tw718140@gmail.com>
2023-11-16 21:14:03 +0800

    project init

---
+++ HyReadBooks/HyReadBooks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HyReadBooks
+//
+//  Created by Bing Bing on 2023/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
